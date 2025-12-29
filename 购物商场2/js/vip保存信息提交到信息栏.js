902bbc4a09981f629f327d22f764479db75b11d6
liuliuk123 <1989233479@qq.com>
2025-12-29 17:33:48 +0800

    Delete 购物商场2 directory

--- 购物商场2/js/vip保存信息提交到信息栏.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load",function(){
-    const clickEle = document.querySelector(".vipUp");
-    clickEle.onclick=function(){
-        const addressEle = document.querySelector(".address");
-        addressEle.style.display="block"
-    }
-})
